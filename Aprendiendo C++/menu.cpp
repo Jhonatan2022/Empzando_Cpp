dd3237c33961b29ba15c19b075b03b6f42e3a8cf
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2022-11-09 00:05:43 +0100

    menu

--- Aprendiendo C++/menu.cpp
+++ Aprendiendo C++/menu.cpp
@@ -1,33 +1,51 @@
 #include <iostream>
-
 using namespace std;
-
-int opcion;
+int a, b,numero,vinicio=0, vfinal=0, acel=0,t=0,op;
 int main(int argc, char const *argv[])
 {
-    system("cls");
-    cout << "1. Sumar" << endl;
-    cout << "2. Restar" << endl;
-    cout << "3. Multiplicar" << endl;
-    cout << "4. Salir"<<endl;
-    cout << "Elije una opcion: ";cin >> opcion;
-
-    switch (opcion)
-    {
-    case 1:
-        cout << "Has elejido Sumar.\n";
-
-    case 2:
-        cout << "Has elejido Restar.\n";
-
-    case 3:
-        cout << "Has elejido Multiplicar.\n";
-    
-    case 4:
-        system("pause");
-        return EXIT_SUCCESS;
-
-    default:
-        cout << "Opcion no valida.\a\n";  
-    }
+cout<<"Digite un la opcion que desea usar \n 1. Calculadora sencilla \n
+2. Tablas de multiplicar \n 3. Calcular la acelarecion de un vehiculo. \n
+Opcion: ";
+cin>> op;
+switch (op)
+{
+case 1:
+//Calculadora sencilla
+cout << "Ingresa el primer numero: ";
+cin >> a;
+cout << "Ingresa el segundo numero: ";
+cin >> b;
+cout <<"La suma de los numeros es: "<< a+b <<endl;
+cout <<"La resta de los numeros es: "<< a-b <<endl;
+cout <<"La multiplicacion de los numeros es: "<< a*b <<endl;
+cout <<"La division de los numeros es: "<< a/b <<endl;
+cout <<"El residuo es: "<< a%b <<endl;
+system("pause");
+return EXIT_SUCCESS;
+case 2://Imprimir tablas de multiplicar de un número ingresado
+cout<<"\nSegundo ejercicio con tablas de multiplicar"<<endl;
+do{
+cout<<"Ingrese un numero para ver su tabla de multiplicar: ";
+cin>>numero;
+}while((numero<1) || (numero>30));
+for(int i=1; i<=10; i++){
+cout<<numero<<" * "<<i<<" = "<<numero * i<<endl;
+}
+system("pause");
+return EXIT_SUCCESS;
+case 3://calcular la velocidad
+cout<<"\nCalcular la aceleracion de un vehiculo: " ;
+cout<<"Introduce la velocidad inicial: ";
+cin>>vinicio;
+cout << "Introduce la velocidad final: ";
+cin>>vfinal;
+cout << "Introduce el tiempo: ";
+cin>>t;
+acel = (vfinal - vinicio)/t;
+cout <<"La acelacion del vehiculo es:\n "<< acel <<endl;
+system("pause");
+return EXIT_SUCCESS;
+default: //No ha dijitado una opcion disponible
+cout<<"No ha elegido una opcion del 1 al 3";
+}
 }
